91e3841a424fd5687134860e44a50d5eeb0aec41
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-03-29 15:05:31 -0700

    Dropdown menu component foundation.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 
 const items = [
@@ -18,11 +19,33 @@ const items = [
     },
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red',
+    },
+    {
+        label: 'The Color Green',
+        value: 'green',
+    },
+    {
+        label: 'A Shade of blue',
+        value: 'blue',
+    }
+]
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
             {/* <Accordion items={items}/> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown 
+                options={options}
+                onSelectedChange={setSelected}
+                selected={selected}
+            />
         </div>
     );
 };
